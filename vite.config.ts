f8dcdacf2a99f080873eebbeef219f3c32355409
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-30 18:53:30 -0500

    added absolute importing

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$src: '/src',
+			$components: '/src/components',
+			$root: '/',
+			$routes: '/src/routes',
+			$utils: '/src/utils',
+			$types: '/src/types',
+			$stores: '/src/stores',
+		},
+	},
 });
